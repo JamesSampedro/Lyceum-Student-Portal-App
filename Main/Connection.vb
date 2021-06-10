4e77e3ed98fc52088642dc477ed410ee5739dd93
James.Sampedro <72242960+Votum38git@users.noreply.github.com>
2021-06-10 17:23:04 +0800

    Add project files.

---
+++ Main/Connection.vb
@@ -0,0 +1,3 @@
+﻿Public Class Connection
+    Public connectionString As String = "Provider=Microsoft.ACE.OLEDB.12.0;Data Source=F:\Downloads\Eclair-Student-Portal-App-master\Eclair Student Portal.accdb"
+End Class
